bd2ab9598ae2eaacd0140342326ada877598f06c
konrad1201 <konradmroz1@tlen.pl>
2021-03-17 00:35:49 +0100

    Test gita cd.

---
+++ Projekt Programowanie Obiektowe/Class1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Programowanie_Obiektowe
+{
+	public class Student
+	{
+		private string Name;
+		private string Surname;
+		private int Department;
+		private int Year;
+		private int Semester;
+		private int Level;
+		private double Average;
+		private double Family_Money;
+		private int Family_Members;
+		private string Bank_Account_Number;
+		private double Distance;
+
+		public string Name_p
+		{
+			get
+			{
+				return Name;
+			}
+
+			set
+			{
+				Name = value;
+			}
+		}
+	
+		public string Surname_p
+		{
+			get
+			{
+				return Surname;
+			}
+
+			set
+			{
+				Surname = value;
+			}
+		}
+
+		public string Bank_Account_Number_p
+		{
+			get
+			{
+				return Bank_Account_Number;
+			}
+
+			set
+			{
+				Bank_Account_Number= value;
+			}
+		}
+
+		public int Department_p
+		{
+			get
+			{
+				return Department;
+			}
+
+			set
+			{
+				Department = value;
+			}
+		}
+
+		public int Year_p
+		{
+			get
+			{
+				return Year;
+			}
+
+			set
+			{
+				Year = value;
+			}
+		}
+
+		public int Semester_P
+		{
+			get
+			{
+				return Semester;
+			}
+
+			set
+			{
+				Semester = value;
+			}
+		}
+
+		public int Level_p
+		{
+			get
+			{
+				return Level;
+			}
+
+			set
+			{
+				Level = value;
+			}
+		}
+	}
+
+}
+
